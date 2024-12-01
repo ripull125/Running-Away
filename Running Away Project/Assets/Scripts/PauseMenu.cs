4f1cb82c4529daf6ef11d892ef537fe4ee9e3d20
EvanC22 <evanchin0322@gmail.com>
2024-12-01 14:26:34 -0500

    got pause menu working

---
+++ Running Away Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Cursor.visible = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMenu()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Main-Menu");
+        isPaused = false;
+    }
+
+    public void QuitGame() 
+    {
+        #if UNITY_EDITOR
+        {
+            EditorApplication.isPlaying = false;
+        }
+        #else
+        {
+            Application.Quit();
+        }
+        #endif
+    }
+}
